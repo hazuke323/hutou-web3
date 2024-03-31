c36fb3cab152eac3f4ec7d37148683998502b100
fimwi <fimdoit@gmail.com>
2024-03-31 12:05:30 +0800

    正式网地址替换

--- src/config/contract.js
+++ src/config/contract.js
@@ -1,9 +1,10 @@
-const IS_TEST = true
+// const IS_TEST = true
+const IS_TEST = false
 
-export const Contract_Addr = IS_TEST ? '0x854a4f6f5227ad6C8Be5c4b89e2b5bd22fAb6f0B' : '0x20d62923Ac342ac21d483323661bF1247151e3C5'
+export const Contract_Addr = IS_TEST ? '0x854a4f6f5227ad6C8Be5c4b89e2b5bd22fAb6f0B' : '0x3799a31D702Bed60ab57CEb719cA75079d2e9854'
 
 export const Contract_USDT_Addr = IS_TEST ? '0x337610d27c682e347c9cd60bd4b3b107c9d34ddd' : '0x55d398326f99059ff775485246999027b3197955'
 
-export const Contract_Hutou_Deposit_Addr = IS_TEST ? '0x019549d75572347ABb6510621AA8E5C9ebA672Cc' : ''
+export const Contract_Hutou_Deposit_Addr = IS_TEST ? '0x019549d75572347ABb6510621AA8E5C9ebA672Cc' : '0x420291C7Dde815EA943aab29b8F87C708BD4D10B'
 
-export const Contract_Hutou_Addr = IS_TEST ? '0x0d867894f34D38Bc0D0f4E60F156392bB722f61E' : ''
+export const Contract_Hutou_Addr = IS_TEST ? '0x0d867894f34D38Bc0D0f4E60F156392bB722f61E' : '0xbc470e3d506df49e3498dac3df57d9dbb746caa6'
